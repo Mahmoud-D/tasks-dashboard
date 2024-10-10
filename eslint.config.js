e9927a49dcdc9ffc64e8e820760dd01e5b9cbda0
Mahmoud Ahmed <Mahmoud.A.Dash@gmail.com>
2024-10-10 23:35:35 +0300

    init

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-hooks", "react-refresh"],
+  rules: {
+    "react-hooks/rules-of-hooks": "error", // Add react-hooks rules
+    "react-hooks/exhaustive-deps": "warn", // Add react-hooks rules
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
