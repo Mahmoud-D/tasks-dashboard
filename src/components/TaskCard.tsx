86c49f085aadccf27baae802833b2d0eb9c8840e
Mahmoud Ahmed <Mahmoud.A.Dash@gmail.com>
2024-10-13 15:20:50 +0300

    feat: Update compoent UI

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,13 +1,26 @@
 import {
   Card,
   CardContent,
-  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { TTask } from "@/types";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 import { Button } from "./ui/button";
+
+import { TTask } from "@/types";
+import { cn } from "@/lib/utils";
+import { FilePenLine, Trash2 } from "lucide-react";
 
 type TProps = {
   task: TTask;
@@ -20,20 +33,60 @@ const TaskCard = ({
   onDelete: handleDelete,
 }: TProps) => {
   return (
-    <Card>
+    <Card className=" w-[300px] sm:w-[400px] ">
       <CardHeader>
-        <CardTitle></CardTitle>
-
-        <CardDescription>{task.status}</CardDescription>
+        <CardTitle className="flex items-center justify-between pb-2 border-b">
+          <span className="">{task.title}</span>
+          <span
+            className={` w-fit rounded-md text-sm px-2 py-1 text-white font-semibold  ${cn(
+              task.status === "in-progress" && "bg-blue-500 ",
+              task.status === "finished" && "bg-green-500",
+              task.status === "not-started" && "bg-orange-300 "
+            )} `}
+          >
+            {task.status}
+          </span>
+        </CardTitle>
       </CardHeader>
-      <CardContent>{task.description}</CardContent>
-      <CardFooter>
-        <Button type="button" onClick={() => handleEdit(task)}>
-          Edit
+      <CardContent className="truncate ">{task.description}</CardContent>
+      <CardFooter className="flex items-center justify-between gap-4 ">
+        <Button
+          className="bg-[#006F9E] hover:bg-[#006F9E] "
+          type="button"
+          onClick={() => handleEdit(task)}
+        >
+          <FilePenLine size={16} />
         </Button>
-        <Button type="button" onClick={() => handleDelete(task)}>
-          Delete
-        </Button>
+        {/* AlertDialog Trigger and Content */}
+        <AlertDialog>
+          <AlertDialogTrigger asChild>
+            <Button className="bg-red-500 hover:bg-red-500" type="button">
+              <Trash2 size={16} />
+            </Button>
+          </AlertDialogTrigger>
+          <AlertDialogContent className=" w-[300px] sm:w-[400px]">
+            <AlertDialogHeader>
+              <AlertDialogTitle>
+                Are you sure you want to delete this task?
+              </AlertDialogTitle>
+
+              <AlertDialogDescription>
+                This action cannot be undone. This will permanently delete the
+                task.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction
+                onClick={() => handleDelete(task)}
+                className="bg-red-500 hover:bg-red-500"
+                type="button"
+              >
+                Confirm
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
       </CardFooter>
     </Card>
   );
